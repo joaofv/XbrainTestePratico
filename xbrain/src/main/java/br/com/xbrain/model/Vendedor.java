80b61e5245059e020e0ad7ac63a4859805409c10
João Vitor <joaofv_@hotmail.com>
2021-02-17 14:46:06 -0300

    Criação da Classe Vendedor

--- xbrain/src/main/java/br/com/xbrain/model/Vendedor.java
+++ xbrain/src/main/java/br/com/xbrain/model/Vendedor.java
@@ -1,5 +1,72 @@
 package br.com.xbrain.model;
 
-public class Vendedor {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "Vendedor")
+public class Vendedor {
+	
+	@Id
+	@Column(name = "vendedorId")
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long idVendedor;
+	
+	@Column(name = "vendedorNome", length = 100, nullable = false)
+	private String nome;
+	
+	public Vendedor(){}
+	
+	public Vendedor(Long idVendedor, String nome){
+		super();
+		this.idVendedor = idVendedor;
+		this.nome = nome;
+	}
+
+	public Long getIdVendedor() {
+		return idVendedor;
+	}
+
+	public void setIdVendedor(Long idVendedor) {
+		this.idVendedor = idVendedor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idVendedor == null) ? 0 : idVendedor.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Vendedor other = (Vendedor) obj;
+		if (idVendedor == null) {
+			if (other.idVendedor != null)
+				return false;
+		} else if (!idVendedor.equals(other.idVendedor))
+			return false;
+		return true;
+	}
+	
+	
 }
