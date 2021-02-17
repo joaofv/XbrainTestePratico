89df36761a5e767e70a5a8971eb62337f1b7163f
João Vitor Félix Favero <57413060+joaofv@users.noreply.github.com>
2021-02-17 03:10:42 -0300

    Criando Projeto API com Spring Boot

---
+++ xbrain/src/test/java/br/com/xbrain/XbrainApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.xbrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XbrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
